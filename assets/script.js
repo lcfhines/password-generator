85c9a90ca633d57f0a011443e7df2fa1729de0bd
lcfhines <113798073+lcfhines@users.noreply.github.com>
2022-10-03 15:42:24 -0400

    added prompt for password length

--- assets/script.js
+++ assets/script.js
@@ -6,6 +6,16 @@ var generateBtn = document.querySelector("#generate");
 
 // FUNCTIONS
 
+// ensure user enters valid password length, show prompt again if not
+
+function getPasswordLength() {
+  var length = parseInt(prompt("Enter your password length (between 8 and 128 characters):"));
+  if ((length < 8 || length > 128) || isNaN(length)){
+    return getPasswordLength();
+  }
+  return length;
+}
+
 // create the generatePassword function that will return our password
 function generatePassword(){
   // different sets of characters - store as data
@@ -17,10 +27,9 @@ var includedCharacters = "";
 var password = "";
 
 // PASSWORD LENGTH
+var passwordLength = getPasswordLength();
 
-// get selection of character types
-
-
+// get selection of character types to include in includedCharacters
 var includeNumbers = confirm("Would you like your password to include numbers?");
 if (includeNumbers) includedCharacters = includedCharacters + numbers;
 
@@ -33,11 +42,8 @@ if (includeUpperCase) includedCharacters = includedCharacters + upperCase;
 var includeSpecialChar = confirm("Would you like your password to include special characters?");
 if (includeSpecialChar) includedCharacters = includedCharacters + specialChar;
 
+return password
 }
-
-  return "password"
-
-
 
 
 // INITIALIZATION
